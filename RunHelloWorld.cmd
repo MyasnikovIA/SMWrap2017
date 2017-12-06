0f7f8d1fb86bb86afd5c5bb3169370628023c73f
MyasnikovIA <miacom@mail.ru>
2017-12-06 10:32:28 +0800

    Update RunHelloWorld.cmd

--- RunHelloWorld.cmd
+++ RunHelloWorld.cmd
@@ -1,1 +1,5 @@
 cmd /c MPlayer.exe -h127.0.0.1 -nUSER -t100 -fUser.HelloWorldFRM -p7200
+rem -h127.0.0.1  - хост сервера "127.0.0.1"
+rem -p7200       - Порт сервера 7200
+rem -nUSER       - Область имен "USER"
+rem -fUser.HelloWorldFRM - класс который мы запускаем  "User.HelloWorldFRM" 
